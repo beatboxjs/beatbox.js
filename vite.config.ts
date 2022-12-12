a6ac61c673d700f949ea9bf826d7b9e886363907
Candid Dauth <cdauth@cdauth.eu>
2022-12-13 00:26:04 +0100

    Do not bundle external "events" dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import dtsPlugin from 'vite-plugin-dts';
+import autoExternalPlugin from 'rollup-plugin-auto-external';
 
 export default defineConfig({
 	plugins: [
-		dtsPlugin()
+		dtsPlugin(),
+		autoExternalPlugin()
 	],
 	build: {
 		sourcemap: true,
